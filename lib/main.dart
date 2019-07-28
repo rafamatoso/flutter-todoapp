39111d2f6c4dee21974ae5dbc6c234aeeb7f01d7
rafamatoso <rafaelmatosog@gmail.com>
2019-07-28 12:08:34 -0300

    Lesson 7 - CheckboxListTitle

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'model/item.dart';
 
-// função main que retorna void
+// Função main que retorna void
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// 6) Classe HomePage foi mudada de Stateless para Stateful pois teremos mudanças de estados variadas.
 class HomePage extends StatefulWidget {
   var items = new List<Item>();
 
@@ -41,14 +42,28 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("ToDo List"),
       ),
-      // O método .builder() renderiza o ListView em tempo de execução, sobre demanda, dando mais eficiência e melhorando a performance do elemento
+      // 6) O método .builder() renderiza o ListView em tempo de execução, sobre demanda, dando mais eficiência e melhorando a performance do elemento
       body: ListView.builder(
-        // O ListView acessará os itens de acordo com a quantidade contida nele próprio
+        // 6) O ListView acessará os itens de acordo com a quantidade contida nele próprio
         itemCount: widget.items.length,
-        // Função que será responsável pela construção dos itens na tela (Como deve ser construido os itens? Como desenho? O que faço?)
+        // 6) Função que será responsável pela construção dos itens na tela (Como deve ser construido os itens? Como desenho? O que faço?)
         itemBuilder: (BuildContext context, int index) {
-          // Retornará o texto na Tela da App
-          return Text(widget.items[index].title);
+          // 7) Criado para desestruturar a chamada direto no return, dessa forma poderemos chamar a variável item por toda a app
+          final item = widget.items[index];
+          // 6) Retornará o texto na Tela da App
+          return CheckboxListTile(
+            // 7) Mostra o título
+            title: Text(item.title),
+            // 7) Tem que apresentar uma chave única
+            key: Key(item.title),
+            // 7) Um booleano, true ou false
+            value: item.done,
+            // 7) Ao clicar, mudará valor
+            onChanged: (value) {
+              // 7) Semelhante ao console.log() do js
+              print(value);
+            },
+          );
         },
       ),
     );
